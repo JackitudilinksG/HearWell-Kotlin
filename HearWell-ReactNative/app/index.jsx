789a7af2325120abf31e18e6fb3ce54322a25aff
Deric Jojo <73828512+JackitudilinksG@users.noreply.github.com>
2025-02-07 21:56:20 +0530

    working audio recording and playback

--- HearWell-ReactNative/app/index.jsx
+++ HearWell-ReactNative/app/index.jsx
@@ -1,37 +1,96 @@
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import React from "react";
-//import { SafeAreaView } from "react-native-safe-area-context";
-import icedCoffee from "@/assets/images/iced-coffee.png";
+import { StyleSheet, Button, View, Text } from "react-native";
+import { Audio } from 'expo-av';
 
-const app = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={icedCoffee} style={styles.image}>
-        <Text style={styles.text}>Coffee Shop</Text>
-      </ImageBackground>
-    </View>
-  );
-};
+export default function index() {
+    const [recording, setRecording] = React.useState();
+    const [recordings, setRecordings] = React.useState([]);
 
-export default app;
+    async function startRecording() {
+        try {
+            const perm = await Audio.requestPermissionsAsync();
+            if(perm.status === "granted") {
+                await Audio.setAudioModeAsync({
+                    allowsRecordingIOS: true,
+                    playsInSilentModeIOS: true
+                });
+                const { recording } = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+                setRecording(recording);
+            }
+        } catch(err) {}
+    }
+
+    async function stopRecording() {
+        setRecording(undefined);
+
+        await recording.stopAndUnloadAsync();
+        let allRecordings = [...recordings];
+        const { sound, status } = await recording.createNewLoadedSoundAsync();
+        allRecordings.push({
+            sound: sound,
+            duration: getDurationFormatted(status.durationMillis),
+            file: recording.getURI()
+        })
+
+        setRecordings(allRecordings);
+    }
+    
+    function clearRecordings() {
+        setRecordings([]);
+    }
+
+    function getDurationFormatted(millis) {
+        const minutes = millis/1000/60;
+        const seconds = Math.round((minutes - Math.floor(minutes))*60);
+        return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+    }
+
+    function getRecordingLines() {
+        return recordings.map((recordingLine, index) => {
+            return (
+                <View key={index} style={styles.row}>
+                    <Text styles={styles.fill}>
+                        Recording #{index+1} | {recordingLine.duration}
+                    </Text>
+                    <Button onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+                </View>
+            );
+        });
+    }
+
+    return (
+        <View style={styles.container}>
+            <Button title={recording ? "Stop Recording" : "Start Recording"} onPress={recording ? stopRecording : startRecording}></Button>
+            {getRecordingLines()}
+            <Button title={recordings.length > 0 ? "Clear Recordings" : ""} onPress={clearRecordings} style={recordings.length > 0 ? styles.exists : styles.nonexists}></Button>
+        </View>
+    );
+
+    
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: 'rgba(0, 0, 0, 0.5)'
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center'
-  }
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: 10,
+        marginRight: 40
+    },
+    fill: {
+        flex: 1,
+        margin: 15
+    },
+    nonexists: {
+        backgroundColor: "#fff"
+    },
+    exists: {
+        backgroundColor: "orange"
+    }
+})
